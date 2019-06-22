109a5b9bd85f9ce178132e898bf8b98fb8ff8652
Moon C.W <52101571+octopusoctopus@users.noreply.github.com>
2019-06-22 20:22:57 +0900

    Add files via upload

---
+++ lesson1.ino
@@ -0,0 +1,54 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(13,OUTPUT);
+  
+  digitalWrite(13,HIGH);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(500);
+  
+  
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(200);
+
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(500);
+
+  
+}
